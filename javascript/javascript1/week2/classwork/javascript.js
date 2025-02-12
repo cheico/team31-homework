8d9e257722a820cad425928d232ba8f171d33685
cheico <nchenzo@yahoo.com>
2025-02-12 15:39:24 +0100

    javascript homework

---
+++ javascript/javascript1/week2/classwork/javascript.js
@@ -0,0 +1,11 @@
+// This function emulates sending emails to receipients
+const emailList = "benjamin@gmail.com|peter@gmail.com|hans@gmail.com|ahmad@gmail.com|sana@gmail.com|virgeen@gmail.com|mohammed@gmail.com"
+function sendEmailTo(recepient) {
+    recepient=emailList.split("|")
+for loop
+
+    
+}
+
+
+
